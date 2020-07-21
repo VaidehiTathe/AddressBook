1b72e9dab8eadf1c8ce0a7b229c557de8ab14c57
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-21 15:26:41 +0530

    [Vaidehi]ADD:person class created

---
+++ AddressBook/AddressBook/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Person 
+    {
+        private string firstName, lastName, address, city, state;
+        private int zip, phoneNumber;
+         public string getFirstName
+         {
+            get { return firstName; }
+            set { firstName = value; }
+         }
+         public string getLastName
+         {
+             get { return lastName; }
+             set { lastName = value; }
+         }
+        public string getAddress
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string getCity
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string getState
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        public int getZip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+        public int getPhoneNumber
+        {
+            get { return phoneNumber; }
+            set => phoneNumber = value;
+        }
+
+    }
+}
