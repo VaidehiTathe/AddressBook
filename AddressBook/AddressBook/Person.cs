414a6ca33671a67e8be537facaa813502c2cf9c9
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-21 20:29:03 +0530

    [Vaidehi]ADD:taking input from user

--- AddressBook/AddressBook/Person.cs
+++ AddressBook/AddressBook/Person.cs
@@ -8,6 +8,32 @@ namespace AddressBook
     {
         private string firstName, lastName, address, city, state;
         private int zip, phoneNumber;
+
+         public void AddPerson()
+        {
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+           
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+           
+            Console.WriteLine("Enter your address:");
+            string address = Console.ReadLine();
+            
+            Console.WriteLine("Enter your city:");
+            string city = Console.ReadLine();
+           
+            Console.WriteLine("Enter your state:");
+            string state = Console.ReadLine();
+            
+            Console.WriteLine("Enter your zip code:");
+            int zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your phoneNumber:");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+        
+            Console.WriteLine("Person information is:{0} {1} {2} {3} {4} {5} {6}", firstName, lastName, address,city,state,zip,phoneNumber);
+        }
          public string FirstName
          {
             get { return firstName; }
